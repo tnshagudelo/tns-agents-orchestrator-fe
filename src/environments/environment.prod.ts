d062ebb01e2e5c1e2d09742003cf8197d8b83705
Hernán Darío Agudelo Guzmán <hernan.agudelo@techandsolve.com>
2026-04-17 23:32:52 -0500

    ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://tns-agents-general-api-b8a4fzczbpe0ard2.canadacentral-01.azurewebsites.net',
   wsUrl: '/ws',
   appVersion: '1.0.0',
   githubOAuth: {
-    clientId: '',
-    redirectUri: '/auth/callback',
+    clientId: 'Ov23liQfnFr6bTMtww0N',
+    redirectUri: 'https://tnsstoragetest.z20.web.core.windows.net/auth/callback',
     scope: 'read:user user:email',
   },
 };
